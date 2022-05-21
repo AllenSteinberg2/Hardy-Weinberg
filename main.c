5faf5d3ed30d63aba6959cd063bddce421eca2ec
Allen Steinberg <adsteinberg01@gmail.com>
2022-05-21 10:43:14 -0500

    Made minor changes and decreased population size, since it seemed to cause problems at POP = 100000

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int POP = 1000000; // Population size
-const int NUM_GENS = 100; // Number of generations simulated
-const float domPercent = 0.2; // Percent of alleles in the population which are dominant
-const char domChar = 'A'; // Character representing dominant allele
-const char resChar = 'a'; // Character representing recessive allele
+#define POP 1000 // Population size
+#define NUM_GENS 100 // Number of generations simulated
+#define domPercent 0.2 // Percent of alleles in the population which are dominant
+#define domChar 'A' // Character representing dominant allele
+#define resChar 'a' // Character representing recessive allele
 
 typedef struct org org;
 struct org{ // Each organism is diploid
@@ -75,8 +75,8 @@ void printPopData(org orgs[]){ // Prints gene pool and genotype statistics
             hDomCount++;
         }
     }
-    printf("\nGene pool: %.2f%% dominant (%c), %.2f%% recessive (%c).", 50.0*alleleDomCount/POP, domChar, 50.0*alleleResCount/POP, resChar);
-    printf("\n Genotypes: %.2f%% homozygous dominant, %.2f%% heterozygous, %.2f%% homozygous recessive.", 100.0*hDomCount/POP, 100.0*hetCount/POP, 100.0*hResCount/POP);
+    printf("Gene pool: %.2f%% dominant (%c), %.2f%% recessive (%c).\n", 50.0*alleleDomCount/POP, domChar, 50.0*alleleResCount/POP, resChar);
+    printf("Genotypes: %.2f%% homozygous dominant, %.2f%% heterozygous, %.2f%% homozygous recessive.\n", 100.0*hDomCount/POP, 100.0*hetCount/POP, 100.0*hResCount/POP);
 }
 
 int main(){
@@ -94,9 +94,11 @@ int main(){
         orgs[i] = newOrg;
     }
     
+    printf("Generation 0: ");
     printPopData(orgs);
     for(int i = 0; i < NUM_GENS; i++){ // Iterate through the generations and see how close they are to H-W equilibrium
         nextGen(orgs);
+        printf("Generation %d: ", i);
         printPopData(orgs);
     }
 
